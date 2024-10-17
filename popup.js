0819f762c6dca2d8faa9ae6307b107242c9a4cd5
Eldohub Academy <academy@eldohub.co.ke>
2024-10-17 12:56:55 +0300

    done

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById("changeColor").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          document.body.style.backgroundColor = "lightblue";
+        },
+      });
+    });
+  });
